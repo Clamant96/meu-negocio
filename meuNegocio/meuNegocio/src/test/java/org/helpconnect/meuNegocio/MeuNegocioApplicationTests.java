a514fd779ed4bd9023965e63768207bda55edb2a
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-31 08:33:00 -0300

    Criacao e upload de projeto spring meuNegocio, um sistema gerenciador de e-commerce

---
+++ meuNegocio/meuNegocio/src/test/java/org/helpconnect/meuNegocio/MeuNegocioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helpconnect.meuNegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuNegocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
